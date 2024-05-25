64375d9edb18861829a28fb813bb175aa358ae0f
chuans <juaazn.05@gmail.com>
2024-05-25 13:24:40 +0200

    added Fetch

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hola");
+const URL = "https://opentdb.com/api.php?amount=20&difficulty=medium&type=multiple";
+
+const options = {
+  method: "GET",
+  mode: "cors",
+  cache: "no-cache",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+fetch(URL, options)
+  .then((res) => res.json())
+  .then((json) => {
+    const { results } = json;
+    console.log(results);
+  })
+  .catch((err) => console.log(err));
