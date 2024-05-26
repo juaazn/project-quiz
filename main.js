4a6890960e5e7c197918d8f0ef1ceaa3eff284ee
chuans <juaazn.05@gmail.com>
2024-05-26 13:06:47 +0200

    Fetch API

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 const URL = "https://opentdb.com/api.php?amount=20&difficulty=medium&type=multiple";
 
-const options = {
-  method: "GET",
-  mode: "cors",
-  cache: "no-cache",
-  headers: {
-    "Content-Type": "application/json",
-  },
-};
-
-fetch(URL, options)
+fetch(URL)
   .then((res) => res.json())
   .then((json) => {
     const { results } = json;
